9ff1a292589c3c654d543801a2a2b4d6bf7bf7df
natav <nvarshavskaya@yahoo.com>
2014-10-25 21:02:26 -0500

    Initial commit.

---
+++ Recycle/Recycle/Index.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Recycle.Index" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>test</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="author" content="Natalya Varshavskaya for Hack4Reno">
+    <link href="Content/Site.css" rel="stylesheet" /> 
+   
+    <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;sensor=false" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        function initialize() {
+
+            // Create an array of styles.
+            var styles = [
+             {
+                  featureType: "administrative.neighborhood",
+                  elementType: "geometry",
+                  stylers: [
+                    { lightness: 100 },
+                    { visibility: "simplified" }
+                  ]
+              }
+            ];
+
+            // Create a new StyledMapType object, passing it the array of styles,
+            // as well as the name to be displayed on the map type control.
+            var styledMap = new google.maps.StyledMapType(styles,
+              { name: "Styled Map" });
+
+            var latlng = new google.maps.LatLng(39.5254004, -119.8135266);
+            var myOptions = {
+                zoom: 12,
+                center: latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+            //Associate the styled map with the MapTypeId and set it to display.
+            map.mapTypes.set('map_style', styledMap);
+            map.setMapTypeId('map_style');
+        }
+
+
+    </script>
+    <style type="text/css">
+        #map_canvas {
+            height: 43px;
+        }
+ 
+    </style>
+</head>
+
+<body onload="initialize()">
+
+<form id="Form1" runat="server">
+    
+    <div>
+        <table style="text-align: center; width: 1157px;">
+             <tr style="align-content:center; width: 100%;"><td style="text-align: center">.::  banner goes here  ::.</td></tr>
+        </table>
+     </div>
+
+    <br />
+    <table>
+        <tr>
+            <td style="width:35%; vertical-align: top;">
+                <div style="vertical-align: top;">
+                    <table>
+                        <tr>
+                            <td colspan="2"><span>Report NO recycling in your building</span></td>
+                        </tr>
+                        <tr>
+                            <td><span>Enter your address: </span></td>
+                            <td><input type="text" id="txtAddress" runat="server" name="txtAddress" style="width: 213px" /></td>
+                        </tr>
+                        <tr>
+                            <td><span>Comment:</span></td>
+                            <td><textarea class="form-control" name="comment" id="txtComment" runat="server" style="width: 213px"></textarea></td>
+                        </tr>
+                        <tr>
+                            <td class="align-right"><input type="submit" value="Submit" class="btn" runat="server" id="btnSubmit" /></td>
+                            <td><label runat="server" id="lblMsg" visible="false">Thank you for your submission!</label></td>
+                        </tr>
+                    </table>
+                </div>
+            </td>
+            <td style="width:65%;">
+                <asp:Literal ID="js" runat="server"></asp:Literal>
+                <div id="map_canvas" style="width: 100%; height: 728px; margin-bottom:2px;"></div>
+            </td>
+        </tr>
+        
+    </table>
+   
+     <br />
+ 
+</form>
+</body>
+</html>
---
+++ Recycle/Recycle/Index.aspx.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Xml;
+using System.Net;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Recycle
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                SubmitAddress();
+            }
+            else
+            {
+                // Connect to the database, get the data
+                string connString = "Data Source=(local);Initial Catalog=dbRecycle;Persist Security Info=True;User ID=daystar;Password=daystar";
+                string query = "select * from tblLocationData";
+
+                SqlConnection conn = new SqlConnection(connString);
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                var dataTable = new DataTable();
+
+                da.Fill(dataTable);
+                conn.Close();
+                da.Dispose();
+                BuildScript(dataTable);
+
+            }
+
+        }
+
+        protected void SubmitAddress()
+        {
+            Address adrs = new Address();
+            adrs.FullAddress = txtAddress.Value.ToString();
+            try
+            {
+                adrs.GeoCode();
+                //Response.Write(adrs.Latitude + "; " + adrs.Longitude);
+
+                // clean the boxes
+                txtAddress.Value = "";
+                txtComment.Value = "";
+                lblMsg.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An Error Occured" + ex.Message);
+            }
+
+        }
+    
+
+    private void BuildScript(DataTable tbl)
+        {
+            String Locations = "";
+            foreach (DataRow r in tbl.Rows)
+            {
+                // bypass empty rows	 	
+                if (r["locLatitude"].ToString().Trim().Length == 0)
+                    continue;
+
+                string Latitude = r["locLatitude"].ToString();
+                string Longitude = r["locLongitude"].ToString();
+
+                // create a line of JavaScript for marker on map for this record	
+                Locations += Environment.NewLine + " map.addOverlay(new GMarker(new GLatLng(" + Latitude + "," + Longitude + ")));";
+            }
+
+            // construct the final script
+            js.Text = @"<script type='text/javascript'>
+                            function initialize() {
+                                if (GBrowserIsCompatible()) {
+                                    var map = new GMap2(document.getElementById('map_canvas'));
+                                    map.setCenter(new GLatLng(39.5254004, -119.8135266), 12); 
+                                    " + Locations + @"
+                                    map.setUIToDefault();
+                                    map.panTo(new GLatLng(39.5254004, -119.8135266));
+                                }
+                            }
+                            </script> ";
+        }       
+}
+    public class Address
+    {
+        public Address()
+        {
+        }
+        //Properties
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string FullAddress { get; set; }
+
+        //The Geocoding here i.e getting the latt/long of address
+        public void GeoCode()
+        {
+            //to Read the Stream
+            StreamReader sr = null;
+
+            //The Google Maps API Either return JSON or XML. We are using XML Here
+            //Saving the url of the Google API 
+            string url = String.Format("http://maps.googleapis.com/maps/api/geocode/xml?address=" + this.FullAddress + "&sensor=false");
+
+            //to Send the request to Web Client 
+            WebClient wc = new WebClient();
+            try
+            {
+                sr = new StreamReader(wc.OpenRead(url));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("The Error Occured" + ex.Message);
+            }
+
+            try
+            {
+                XmlTextReader xmlReader = new XmlTextReader(sr);
+                bool latread = false;
+                bool longread = false;
+
+                while (xmlReader.Read())
+                {
+                    xmlReader.MoveToElement();
+                    switch (xmlReader.Name)
+                    {
+                        case "lat":
+
+                            if (!latread)
+                            {
+                                xmlReader.Read();
+                                this.Latitude = xmlReader.Value.ToString();
+                                latread = true;
+
+                            }
+                            break;
+                        case "lng":
+                            if (!longread)
+                            {
+                                xmlReader.Read();
+                                this.Longitude = xmlReader.Value.ToString();
+                                longread = true;
+                            }
+
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An Error Occured" + ex.Message);
+            }
+        }
+    }
+}
